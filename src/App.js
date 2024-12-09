e146ef7b2068822a5b5dde3a7604506a82ce8db1
chounan tep <tch23125@gmail.com>
2024-12-09 14:16:30 +0700

    push code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TicTacToe from './components/TicTacToe';
+
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/GameOver.jsx
@@ -0,0 +1,7 @@
+function GameOver() {
+    return ( <div>
+        
+    </div> );
+}
+
+export default GameOver;
